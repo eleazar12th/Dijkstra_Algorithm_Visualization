5f079f878b2ba153e367e2c20c19e6651cd654dd
Evgeny Krasnov <evgeny2krasnov@gmail.com>
2023-04-03 19:13:16 +0300

    added change language button

--- first_prototype/script.js
+++ first_prototype/script.js
@@ -79,3 +79,34 @@ playButton.onclick = function(evt) {
         isOnPause = true;
     }
 };
+
+let langSelect = document.querySelector('.lang-select');
+let algorithmStepDescription = document.querySelector('.algorithm-step p');
+let ruText = 'distance(start, v) - длинна кратчайшего из (известных нам на данный момент)\n' +
+    '                    путей от начальной вершины start до вершины v. По ходу работы алгоритма\n' +
+    '                    значения в distance  будут обновляться. Изначально distance(start, v) = 0,\n' +
+    '                    а для всех остальных вершин длинна пути равна бесконечности.\n' +
+    '                    Синим цветом обозначим номера вершин. Внутри каждой вершины v будем записывать\n' +
+    '                    текущее значение distance(start, v).\n' +
+    '                    Обработанными будем называть вершины, для которых distance больше не будет\n' +
+    '                    обновляться. То есть те, для которых мы уже нашли кратчайший возможный путь.\n' +
+    '                    В начале, конечно, все вершины необработанные.';
+let enText = 'distance(start, v) - the length of the shortest (currently known to us)' +
+    ' path from the initial vertex start to the vertex v. As the algorithm works,' +
+    ' the values in distance will be updated. Initially distance(start, v) = 0,' +
+    ' and for all other vertices the path length is equal to infinity.\n' +
+    '    In blue we denote the numbers of vertices.' +
+    ' Inside each vertex v we will write the current value of distance(start, v).\n' +
+    '    Processed vertices are vertices for which distance is no longer updated.' +
+    ' That is, those for which we have already found the shortest possible path.' +
+    ' In the beginning, of course, all vertices are unprocessed.';
+
+langSelect.onchange = function () {
+    if (langSelect.value === 'en') {
+        algorithmStepDescription.setAttribute('lang', 'en');
+        algorithmStepDescription.textContent = enText;
+    } else {
+        algorithmStepDescription.setAttribute('lang', 'ru');
+        algorithmStepDescription.textContent = ruText;
+    }
+};
